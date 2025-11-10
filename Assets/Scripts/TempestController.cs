9596fc32748be3f192c696f8cc298733ba4f8664
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-11-10 10:43:02 -0800

    added layers and vertex animation

---
+++ Assets/Scripts/TempestController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TempestController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {
+        
+    }
+}
