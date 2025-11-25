b0ccefe1dba1f80808a41fdcbb14a96584284b9b
Kevin Tan <astral.abyss7@gmail.com>
2025-11-25 14:09:13 -0800

    Revert "Merge branch 'npc-movement'"
    This reverts commit 852839abaae9e639dbc76b8e19d75b04224a73e7, reversing
    changes made to 420a883052852c06218b38754c9f84dcee99ced7.

--- Assets/Scripts/AbsorbRange.cs
+++ Assets/Scripts/AbsorbRange.cs
@@ -6,43 +6,50 @@ public class AbsorbRange : MonoBehaviour
     [SerializeField] private TempestMain selfTempest;
     [SerializeField] private List<TempestMain> list = new List<TempestMain>();
 
-    [SerializeField] private SphereCollider col;
-    public float range;
+    [SerializeField] private BoxCollider col;
 
-    private SphereCollider sc;
+    private float stabilityDamageTimer = 0f;
+
+
     private void Awake()
     {
-        sc = GetComponent<SphereCollider>();
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
         CheckAbsorbList();
-        col.radius = selfTempest.size * 2.5f;
-        range = col.radius;
+        col.size = Vector3.one * (selfTempest.size * 3f);
     }
 
     private void CheckAbsorbList()
     {
         foreach (TempestMain tempest in new List<TempestMain>(list))
         {
-            if (tempest == null)
+            stabilityDamageTimer += Time.deltaTime;
+            if (stabilityDamageTimer > 1f)
             {
-                list.Remove(tempest);
+                tempest.ModifyStability(-selfTempest.stabilityDamageRate);
+                stabilityDamageTimer = 0f;
             }
-            else if (tempest.size < selfTempest.size) 
+
+            if (IsAbsorbable(tempest)) 
             {
                 selfTempest.ChangeSize(tempest.size);
                 list.Remove(tempest);
                 tempest.GetAbsorbed();
             }
         }
+    }
+
+    public bool IsAbsorbable(TempestMain tempest)
+    {
+        if (tempest.size < selfTempest.size && tempest.Stability <= selfTempest.stabilityAbsorbThreshold)
+        {
+            return true;
+        }
+        return false;
     }
 
     private void OnTriggerEnter(Collider other)
--- Assets/Scripts/NPC_Tempest.cs
+++ Assets/Scripts/NPC_Tempest.cs
@@ -1,180 +1,37 @@
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.AI;
-
-/*
-this feels so stupid but on the lowkes i feel a bit smarr
-*/
 
 public class NPC_Tempest : MonoBehaviour
 {
-    [Header("Refereneces")]
-    [SerializeField] private TempestMain selfTempest;
-    [SerializeField] private TargetSensor targetSensor;
-    [SerializeField] private AbsorbRange absorbRange;
-    [SerializeField] private NavMeshAgent agent;
-    
-        // sorted by closest to farthest
-    [SerializeField] private List<GameObject> predators;
-    [SerializeField] private List<GameObject> prey;
+    [SerializeField] private TempestMain tempest;
+    [SerializeField] private GameObject player;
+    [SerializeField] private AbsorbRange absorb;
+
+
+    private TempestMain playerTempest;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player_Tempest");
+        playerTempest = player.GetComponent<TempestMain>();
+    }
 
     private void Start()
     {
-        selfTempest = GetComponent<TempestMain>();
-        // selfTempest.size = Random.Range(selfTempest.level1Threshold, selfTempest.maxSize);
-        selfTempest.coreColor = selfTempest.colorList[Random.Range(0, selfTempest.colorList.Count)];
-
-        predators = new List<GameObject>();
-        prey = new List<GameObject>();
-
-        targetSensor = GetComponentInChildren<TargetSensor>();
-        targetSensor.OnTargetEnter += HandleTempestEntered;
-        targetSensor.OnTargetExit += HandleTempestExited;
-        selfTempest.OnSizeChange += HandleSelfGrowth;
-        selfTempest.OnAbsorbed += HandleSelfDeath;
-
-        agent.SetDestination(RandomPoint(targetSensor.range));
+        tempest.size = Random.Range(tempest.level1Threshold, tempest.level2Threshold);
     }
 
-    void Update()
+    private void Update()
     {
-        foreach (GameObject predator in predators.ToList())
+        if (player != null)
         {
-            if (predator == null)
-                predators.Remove(predator);
-        }
-        foreach (GameObject _prey in prey.ToList())
-        {
-            if (_prey == null)
-                prey.Remove(_prey);
-        }
-
-        // MOVEMENT
-        /* https://www.youtube.com/watch?v=7EALNQ9tFlw */
-        agent.speed = -selfTempest.speed * 20;
-
-        // chase nearby prey otherwise wander
-        if (prey.Count == 0)
-        {
-            if (agent.remainingDistance <= agent.stoppingDistance)
-                agent.SetDestination(RandomPoint(targetSensor.range));
-            return;
-        }
-
-        agent.SetDestination(prey[0].transform.position);
-
-        // continue chase / wander but avoid predators
-        if (predators.Count == 0)
-            return;
-
-        Vector3 dirToClosestPredator = predators[0].transform.position - transform.position;
-        agent.velocity = Vector3.Lerp(
-            a: agent.desiredVelocity,
-            b: -dirToClosestPredator.normalized * agent.speed * 1.5f,
-            t: Mathf.Clamp01(targetSensor.range - dirToClosestPredator.magnitude
-                / absorbRange.range)
-        );
-    }
-
-    Vector3 RandomPoint(float range)
-    {
-        Vector3 randomPoint = transform.position + (Random.insideUnitSphere * range);
-        NavMeshHit hit;
-
-        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
-        {
-            return hit.position;
-        }
-        return transform.position;
-    }
-
-    void HandleTempestEntered(GameObject target)
-    {
-        if (target.TryGetComponent<TempestMain>(out TempestMain targetTempest))
-        {
-            targetTempest.OnSizeChange += HandleOtherGrowth;
-            targetTempest.OnAbsorbed += HandleOtherDeath;
-
-            if (targetTempest.size >= selfTempest.size)
+            if (playerTempest.size > tempest.size && tempest.Stability <= tempest.stabilityAbsorbThreshold)
             {
-                predators.Add(target);
-                SortByDistance(predators);
+                tempest.coreColor = tempest.absorbableEnemyColor;
             }
             else
             {
-                prey.Add(target);
-                SortByDistance(prey);
+                tempest.coreColor = tempest.unabsorbableEnemyColor;
             }
         }
     }
-
-    void HandleTempestExited(GameObject target)
-    {
-        if (target.TryGetComponent<TempestMain>(out TempestMain tempest))
-        {
-            tempest.OnSizeChange -= HandleOtherGrowth;
-            tempest.OnAbsorbed -= HandleOtherDeath;
-        }
-       
-
-        try
-        {
-            predators.Remove(target); 
-            prey.Remove(target);
-        }
-        catch{}
-    }
-
-    void HandleSelfGrowth(GameObject self, float newSize)
-    {
-        foreach (GameObject tempest in predators.ToList())
-        {
-            if (tempest.GetComponent<TempestMain>().size < newSize)
-            {
-                predators.Remove(tempest);
-                prey.Add(tempest);
-            }
-        }
-
-        SortByDistance(prey);
-    }
-
-    void HandleOtherGrowth(GameObject target, float newSize)
-    {
-        if (newSize > selfTempest.size)
-        {
-            prey.Remove(target);
-            predators.Add(target);
-        }
-
-        SortByDistance(predators);
-    }
-
-    void HandleSelfDeath(GameObject deceased)
-    {
-        targetSensor.OnTargetEnter -= HandleTempestEntered;
-        targetSensor.OnTargetExit -= HandleTempestExited;
-        selfTempest.OnSizeChange -= HandleSelfGrowth;
-        selfTempest.OnAbsorbed -= HandleSelfDeath;
-    }
-
-    void HandleOtherDeath(GameObject deceased)
-    {
-        TempestMain tempest = deceased.GetComponent<TempestMain>();
-        tempest.OnSizeChange -= HandleOtherGrowth;
-        tempest.OnAbsorbed -= HandleOtherDeath;
-    }
-
-    void SortByDistance(in List<GameObject> list)
-    {
-        list.Sort(comparison: (a, b) =>
-            Vector3.SqrMagnitude(vector:b.transform.position - transform.position)
-                .CompareTo(
-                    Vector3.SqrMagnitude(vector:a.transform.position - transform.position)
-                )
-        );
-    }
-
-    
 }
